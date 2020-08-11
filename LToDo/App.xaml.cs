4ba2de448054b4adbcfb1c7901d86ae3e9c015ce
LinXiaoYi <874183200@qq.com>
2020-08-11 23:19:29 +0800

    增加任务栏

--- LToDo/App.xaml.cs
+++ LToDo/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Hardcodet.Wpf.TaskbarNotification;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,12 @@ namespace LToDo
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            _taskbar = (TaskbarIcon)FindResource("Taskbar");
+            base.OnStartup(e);
+        }
+
+        private TaskbarIcon _taskbar;
     }
 }
---
+++ LToDo/ViewModel/NotifyIconViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+
+namespace LToDo
+{
+    public class NotifyIconViewModel
+    {
+        /// <summary>
+        /// 设置
+        /// </summary>
+        public ICommand SettingCommand
+        {
+            get
+            {
+                return new DelegateCommand
+                {
+                    
+                };
+            }
+        }
+
+        /// <summary>
+        /// 关闭软件
+        /// </summary>
+        public ICommand ExitApplicationCommand
+        {
+            get
+            {
+                return new DelegateCommand { CommandAction = () => Application.Current.Shutdown() };
+            }
+        }
+    }
+
+    public class DelegateCommand : ICommand
+    {
+        public Action CommandAction { get; set; }
+        public Func<bool> CanExecuteFunc { get; set; }
+
+        public void Execute(object parameter)
+        {
+            CommandAction();
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return CanExecuteFunc == null || CanExecuteFunc();
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+    }
+}
