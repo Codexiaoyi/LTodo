9e87fd0ee81a6884c9d73362bfca5aeac3276e2e
LinXiaoYi <874183200@qq.com>
2020-09-02 22:58:57 +0800

    修改测试项目

--- LTodo.Web/Migrations/20200902072320_AddUserLevel.Designer.cs
+++ LTodo.Web/Migrations/20200902072320_AddUserLevel.Designer.cs
@@ -2,14 +2,16 @@
 using LTodo.Web.Model;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace LTodo.Web.Migrations
 {
     [DbContext(typeof(LDbContext))]
-    partial class LDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200902072320_AddUserLevel")]
+    partial class AddUserLevel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ LTodo.Web/Migrations/20200902072320_AddUserLevel.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LTodo.Web.Migrations
+{
+    public partial class AddUserLevel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Level",
+                table: "Users",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Level",
+                table: "Users");
+        }
+    }
+}
